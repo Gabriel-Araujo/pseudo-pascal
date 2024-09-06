f1d6cd5f9259aecb28ba9b7a27f2e24349a85af8
Gabriel <gabrielsouzacruzaraujo@gmail.com>
2024-09-06 12:51:31 -0300

    implementação da verificação de tipos

--- src/common/symbol.rs
+++ src/common/symbol.rs
@@ -3,37 +3,72 @@ use std::fmt::Formatter;
 use crate::common::token::Token;
 use crate::common::token::TokenType::Identifier;
 
-#[derive(PartialEq, Debug)]
+#[derive( Debug, Clone)]
 pub enum Symbol {
-    Token(Token),
+    Identifier(SymbolIdentifier),
     EOS // End of Scope
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct SymbolIdentifier {
+    pub token: Token,
+    pub identifier_type: String,
 }
 
 impl fmt::Display for Symbol {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}",
         match self {
-            Symbol::Token(t) => t.get_lexeme(),
+            Symbol::Identifier(t) => t.token.get_lexeme(),
             Symbol::EOS => "EOS"
         })
     }
 }
 
+impl PartialEq for Symbol {
+    fn eq(&self, other: &Self) -> bool {
+        if self.is_eos() && other.is_eos() { true }
+        else if self.is_identifier() && other.is_identifier() { 
+            self.as_token().unwrap().get_lexeme() == other.as_token().unwrap().get_lexeme()
+        }
+        else { false }
+    }
+}
+
 impl Symbol {
+    pub fn new(token: Token) -> Self {
+        Symbol::Identifier(SymbolIdentifier{token, identifier_type: "".to_string()})
+    }
+    
     pub fn as_token(&self) -> Option<Token> {
         match self {
-            Symbol::Token(t) => Some(t.to_owned()),
+            Symbol::Identifier(t) => Some(t.token.to_owned()),
             Symbol::EOS => None
         }
     }
+    
+    pub fn change_type(&mut self, new_type: &str) {
+        match self {
+            Symbol::Identifier(t) => {t.identifier_type = new_type.to_string();}
+            Symbol::EOS => {}
+        }
+    }
+    
     pub fn is_eos(&self) -> bool {
         matches!(self, Symbol::EOS)
     }
 
     pub fn is_identifier(&self) -> bool {
         match self {
-            Symbol::Token(t) => t.is_type_of(Identifier),
+            Symbol::Identifier(t) => t.token.is_type_of(Identifier),
             Symbol::EOS => false
+        }
+    }
+    
+    pub fn get_type(&self) -> Option<String> {
+        match self {
+            Symbol::Identifier(t) => {Some(t.identifier_type.to_owned())}
+            Symbol::EOS => {None}
         }
     }
 
--- src/syntactic/mod.rs
+++ src/syntactic/mod.rs
@@ -1,13 +1,15 @@
 use std::error::Error;
 use crate::common::symbol::Symbol;
-use crate::common::symbol::Symbol::{Token as TokenSymbol, EOS};
+use crate::common::symbol::Symbol::{EOS};
 use crate::common::token::{Token, TokenType};
 use crate::common::token::TokenType::*;
 
 pub struct Parser {
     tokens_buffer: Vec<Token>,
     symbol_table: Vec<Symbol>,
+    control_type_stack: Vec<String>,
     program_name: String,
+    amount: usize,
 }
 
 impl Parser {
@@ -17,7 +19,9 @@ impl Parser {
         Self {
             tokens_buffer: temp,
             symbol_table: vec![],
+            control_type_stack: vec![],
             program_name: "".to_string(),
+            amount: 0,
         }
     }
 
@@ -53,7 +57,7 @@ impl Parser {
         self.symbol_table.push(EOS); // Criação do escopo global
         let token = self.consume_identifiers()?;
         self.program_name = token.get_lexeme().to_string();
-        self.add_symbol(TokenSymbol(token))?;
+        self.add_symbol(Symbol::new(token))?;
         
         self.consume(Delimiter, ";")?;
 
@@ -77,7 +81,8 @@ impl Parser {
 
         self.consume(Delimiter, ":")?;
         
-        self.types()?;
+        let symbol_type = self.types()?;
+        self.update_symbols_type(&symbol_type);
 
         self.consume(Delimiter, ";")?;
         
@@ -93,7 +98,8 @@ impl Parser {
 
             self.consume(Delimiter, ":")?;
             
-            self.types()?;
+            let symbol_type = self.types()?;
+            self.update_symbols_type(&symbol_type);
 
             self.consume(Delimiter, ";")?;
             
@@ -104,9 +110,12 @@ impl Parser {
     
     fn list_of_identifiers(&mut self) -> Result<(), String> {
         let token = self.consume_identifiers()?;
-        self.add_symbol(TokenSymbol(token))?; // Adiciona o identificador à tabela de símbolos
+        
+        self.amount += 1;
+        self.add_symbol(Symbol::new(token))?; // Adiciona o identificador à tabela de símbolos
 
         self.list_of_identifiers_prime()?;
+        
         Ok(())
     }
     
@@ -116,7 +125,8 @@ impl Parser {
         if comma.get_lexeme() == "," { 
             self.tokens_buffer.pop();
             let token = self.consume_identifiers()?;
-            self.add_symbol(TokenSymbol(token))?; // Adiciona o identificador à tabela de símbolos
+            self.amount += 1;
+            self.add_symbol(Symbol::new(token))?; // Adiciona o identificador à tabela de símbolos
 
             self.list_of_identifiers_prime()?;
         }
@@ -124,9 +134,9 @@ impl Parser {
         Ok(())
     }
     
-    fn types(&mut self) -> Result<(), String> {
+    fn types(&mut self) -> Result<String, String> {
         match self.tokens_buffer.pop() {
-            None => { return Err("Syntactic Error. Unexpected end of file.".to_string()); }
+            None => { Err("Syntactic Error. Unexpected end of file.".to_string()) }
             Some(value) => {
                 if !value.is_type_of(Keyword) {
                     return Err(format!("Expected an keyword: 'integer', 'real' or 'boolean'.\nInstead got '{}' of type '{}' at line {} column {}.",
@@ -137,9 +147,9 @@ impl Parser {
                     return Err(format!("Expected 'integer', 'real' or 'boolean' got '{}' at line {} column {}.",
                     lexeme, value.get_line(), value.get_column()));
                 }
+                Ok(lexeme.to_owned())
             }
         }
-        Ok(())
     }
     
     // Aqui começa a produção de subprograms_declaration 
@@ -160,7 +170,7 @@ impl Parser {
         self.consume(Keyword, "procedure")?;
         
         let token = self.consume_identifiers()?;
-        self.add_symbol(TokenSymbol(token))?;
+        self.add_symbol(Symbol::new(token))?;
         self.symbol_table.push(EOS);
         self.arguments()?;
 
@@ -291,7 +301,9 @@ impl Parser {
 
     fn command_prime(&mut self) -> Result<(), String> {
         let token = self.consume_identifiers()?;
-        self.symbol_exists(&Symbol::Token(token))?;
+        let temp = self.find_symbol(&Symbol::new(token))?;
+        
+        self.control_type_stack.push(temp.get_type().unwrap());
         self.command_dual_prime()?;
         Ok(())
     }
@@ -302,8 +314,10 @@ impl Parser {
         if next.is_type_of(Assignment) {
             self.consume(Assignment, ":=")?;
             self.expression()?;
+            self.check_atribuation(next.get_line(), next.get_column())?;
         }
         else if next.get_lexeme() == "(" {
+            self.control_type_stack.pop(); // Pra caso não seja um assignment
             self.consume(Delimiter, "(")?;
             self.list_of_expressions()?;
             self.consume(Delimiter, ")")?;
@@ -312,12 +326,13 @@ impl Parser {
             return Err(format!("Invalid operator. Got '=' at line {} column {}, didn't you mean ':='?"
             , next.get_line(), next.get_column()))
         }
+        // self.control_type_stack.pop(); // Pra caso não seja um assignment
         Ok(())
     }
 
     fn procedure_activation(&mut self) -> Result<(), String> {
         let token = self.consume_identifiers()?;
-        self.symbol_exists(&Symbol::Token(token))?;
+        self.find_symbol(&Symbol::new(token))?;
         self.procedure_activation_prime()?;
         Ok(())
     }
@@ -361,6 +376,7 @@ impl Parser {
         if next.is_type_of(RelationalOperators) {
             self.relational_op()?;
             self.simple_expression()?;
+            self.check_relational(next.get_line(), next.get_column())?; // TODO
         }
 
         Ok(())
@@ -387,6 +403,7 @@ impl Parser {
         if next.is_type_of(AdditiveOperators) {
             self.additive_op()?;
             self.term()?;
+            self.check_arithmetics(next.get_line(), next.get_column())?;
             self.simple_expression_prime()?;
         }
         Ok(())
@@ -414,9 +431,13 @@ impl Parser {
     fn term_prime(&mut self) -> Result<(), String> {
         let next = self.peek()?;
         if next.is_type_of(MultiplicativeOperators) {
-            self.multiplicative_op()?;
+            let operator = self.multiplicative_op()?;
             self.factor()?;
             self.term_prime()?;
+            if operator.get_lexeme() == "and" { 
+                self.check_logic(next.get_line(), next.get_column())?; 
+            }
+            else { self.check_arithmetics(next.get_line(), next.get_column())?; }
         }
         Ok(())
     }
@@ -426,15 +447,21 @@ impl Parser {
         
         if next.is_type_of(Identifier) { 
             self.procedure_activation()?;
+            let temp = self.find_symbol(&Symbol::new(next.to_owned()))?;
+            let symbol_type = temp.get_type().unwrap();
+            self.control_type_stack.push(symbol_type);
         }
         else if next.is_type_of(Integer) {
             self.consume_by_type(Integer)?;
+            self.control_type_stack.push("integer".to_string());
         }
         else if next.is_type_of(Real) {
             self.consume_by_type(Real)?;
+            self.control_type_stack.push("real".to_string());
         }
         else if next.is_type_of(Boolean) {
             self.consume_by_type(Boolean)?;
+            self.control_type_stack.push("boolean".to_string());
         }
         else if next.get_lexeme() == "(" {
             self.consume(Delimiter, "(")?;
@@ -443,7 +470,9 @@ impl Parser {
         }
         else if next.is_type_of(Keyword) && next.get_lexeme() == "not" {
             self.consume(Keyword, "not")?;
+            self.control_type_stack.push("boolean".to_string());
             self.factor()?;
+            self.check_logic(next.get_line(), next.get_column())?;
         }
         Ok(())
     }
@@ -453,14 +482,12 @@ impl Parser {
         Ok(())
     }
 
-    fn additive_op(&mut self) -> Result<(), String> {
-        self.consume_by_type(AdditiveOperators)?;
-        Ok(())
+    fn additive_op(&mut self) -> Result<Token, String> {
+        self.consume_by_type(AdditiveOperators)
     }
 
-    fn multiplicative_op(&mut self) -> Result<(), String> {
-        self.consume_by_type(MultiplicativeOperators)?;
-        Ok(())
+    fn multiplicative_op(&mut self) -> Result<Token, String> {
+        self.consume_by_type(MultiplicativeOperators)
     }
 
 }
@@ -518,6 +545,7 @@ impl Parser {
         let mut buffer = self.symbol_table.iter().rev();
         let mut buffer_symbol = buffer.next().unwrap();
 
+        let a = buffer_symbol != &EOS;
         while buffer_symbol != &EOS {
             if buffer_symbol == &symbol {
                 return Err(format!("Identifier '{}' already declared in line {} column {}.",
@@ -530,7 +558,17 @@ impl Parser {
         Ok(())
     }
     
-    fn symbol_exists(&self, symbol: &Symbol) -> Result<(), String> {
+    fn update_symbols_type(&mut self, symbol_type: &str) {
+        let mut buffer = self.symbol_table.iter_mut().rev();
+        let mut buffer_item = buffer.next().expect("Something went wrong while updating symbols table type.");
+        while self.amount > 0 {
+            buffer_item.change_type(symbol_type);
+            buffer_item = buffer.next().expect("Something went wrong while updating symbols table type.");
+            self.amount -= 1;
+        }
+    }
+    
+    fn find_symbol(&self, symbol: &Symbol) -> Result<Symbol, String> {
         if symbol == &EOS { panic!("Wrong use of End of Scope"); }
         let temp = symbol.as_token().unwrap();
         
@@ -538,12 +576,14 @@ impl Parser {
             return Err(format!("Use of the program name at line {} column {}.", temp.get_line(), temp.get_column()))
         }
         
-        match self.symbol_table.iter().find(|item| item == &symbol) {
+        match self.symbol_table.iter().rfind(|item| item == &symbol) {
             None => {
                 Err(format!("Use of the undeclared identifier '{}' at line {} column {}.",
                             temp.get_lexeme(), temp.get_line(), temp.get_column()))
             }
-            Some(_) => { Ok(()) }
+            Some(t) => { 
+                Ok(t.to_owned()) 
+            }
         }
         
     }
@@ -552,5 +592,84 @@ impl Parser {
         loop {
             if self.symbol_table.pop() == Some(EOS) {break;}
         }
+    }
+
+    fn check_arithmetics(&mut self, line: usize, column: usize) -> Result<(), String>{
+        let first = match self.control_type_stack.pop() {
+            None => return Err(format!("Unable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        let second = match self.control_type_stack.pop() {
+            None => return Err(format!("Unable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+
+        if (first == "integer" || first == "real")&& (first == second) { self.control_type_stack.push(first); }
+        else if first == "integer" && second == "real" { self.control_type_stack.push(second); }
+        else if first == "real" && second == "integer" {self.control_type_stack.push(first); }
+        else {
+            return Err(format!("Invalid type between operands in arithmetic operation at line {} column {}.\n\
+                                Cannot execute arithmetic operations between '{}' and '{}'.",
+                               line, column, second, first))
+        };
+
+        Ok(())
+    }
+    
+    fn check_relational(&mut self, line: usize, column: usize) -> Result<(), String> {
+        let first = match self.control_type_stack.pop() {
+            None => return Err(format!("Relational check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        let second = match self.control_type_stack.pop() {
+            None => return Err(format!("Relational check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        
+        if (first == "integer" || first == "real") && (second == "integer" || second == "real") { 
+            self.control_type_stack.push("boolean".to_string());
+            return Ok(());
+        }
+
+        Err(format!("Invalid type between operands in relational operation at line {} column {}.\n\
+                    Cannot execute relational operations between '{}' and '{}'.",
+                    line, column, second, first))
+    }
+    
+    fn check_atribuation(&mut self, line: usize, column: usize) -> Result<(), String> {
+        let first = match self.control_type_stack.pop() {
+            None => return Err(format!("Assignment check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        let second = match self.control_type_stack.pop() {
+            None => return Err(format!("Assignment check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        if first != second { 
+            return Err(format!("Invalid assignment at line {} column {}.\n\
+                                Cannot assign value of type '{}' to an variable of type '{}'.",
+            line, column, first, second))
+        }
+        Ok(())
+    }
+    
+    fn check_logic(&mut self, line: usize, column: usize) -> Result<(), String> {
+        let first = match self.control_type_stack.pop() {
+            None => return Err(format!("Logic check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        let second = match self.control_type_stack.pop() {
+            None => return Err(format!("Logic check Failed.\nUnable to check type at line {} column {}", line, column)),
+            Some(t) => t,
+        };
+        
+        if first != "boolean" || second != "boolean" {
+            return Err(format!("Invalid type between operands in logic operation at line {} column {}.\n\
+                    Cannot execute logic operations between '{}' and '{}'.",
+                        line, column, second, first));
+        }
+        
+        self.control_type_stack.push(first);
+        Ok(())
     }
 }
